f39852ba9e858605eb1f8f2aaac183f4ceaae39d
MayurDuduka <mayur@iamops.io>
2023-08-16 18:55:35 +0530

    fix of shell

--- index.sh
+++ index.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+SOCI_USER="awslabs"
+SOCI_REPO="soci-snapshotter"
+
+set -e
+
 shopt -s expand_aliases
 if [ -z "$NO_COLOR" ]; then
     alias log_info="echo -e \"\033[1;32mINFO\033[0m:\""
@@ -7,63 +13,54 @@ else
     alias log_info="echo \"INFO:\""
     alias log_error="echo \"ERROR:\""
 fi
-set -e
 
-SOCI_USER="awslabs"
-SOCI_REPO="soci-snapshotter"
+RESPONSE=$(curl -s "https://api.github.com/repos/${SOCI_USER}/${SOCI_REPO}/releases/latest")
 
-response=$(curl -s "https://api.github.com/repos/${SOCI_USER}/${SOCI_REPO}/releases/latest")
+LATEST_VERSION=$(echo "$RESPONSE" | jq -r '.tag_name')
 
-LATEST_VERSION=$(echo "$response" | jq -r '.tag_name')
-
-
-if [[ "$RUNNER_OS" == "Linux" ]]; then
-    if [[ "${{ runner.arch }}" == "x64" ]]; then
-        log_info "Runner OS: Linux (x64)"
-        wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz && tar -xvf ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz && cp ./soci /usr/local/bin && cp ./soci-snapshotter-grpc /usr/local/bin
-    elif [[ "${{ runner.arch }}" == "ARM64" ]]; then
-        log_info "Runner OS: Linux (ARM64)"
-        wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-arm64.tar.gz && tar -xvf ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-arm64.tar.gz && cp ./soci /usr/local/bin && cp ./soci-snapshotter-grpc /usr/local/bin
-    else
-        log_error "Runner OS: Linux (Unknown Architecture: ${{ runner.arch }})"
+case ${{ runner.os }} in
+    Linux)
+    case ${{ runner.arch }} in
+        X64)
+            wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz
+            wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz.sha256sum
+            sha256sum -c  ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz.sha256sum;
+            tar -xvf ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz && cp ./soci /usr/local/bin && cp ./soci-snapshotter-grpc /usr/local/bin
+            ;;
+        ARM64)
+            wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-arm64.tar.gz
+            wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-arm64.tar.gz.sha256sum
+            sha256sum -c  ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-arm64.tar.gz.sha256sum;
+            tar -xvf ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-arm64.tar.gz && cp ./soci /usr/local/bin && cp ./soci-snapshotter-grpc /usr/local/bin
+            ;;
+        *)
+            log_error "unsupported architecture $arch"
+            exit 1
+            ;;
+    esac
+    ;;
+    *)
+        log_error "unsupported OS ${{ runner.os }}"
+        log_error "Only Linux binaries are available"
         exit 1
-    fi
-elif [[ "$RUNNER_OS" == "Windows" ]]; then
-    if [[ "${{ runner.arch }}" == "x64" ]]; then
-        log_info "Runner OS: Windows (x64)"
-        exit 1
-    else
-        log_error "Runner OS: Windows (Unknown Architecture: ${{ runner.arch }})"
-        exit 1
-    fi
-elif [[ "$RUNNER_OS" == "macOS" ]]; then
-    if [[ "${{ runner.arch }}" == "x64" ]]; then
-        log_info "Runner OS: macOS (x64)"
-        exit 1
-    else
-        log_error "Runner OS: macOS (Unknown Architecture: ${{ runner.arch }})"
-        exit 1
-    fi
-else
-    log_error "Unknown Runner OS: $RUNNER_OS"
-    log_error "Only Linux binaries are available"
-fi
+        ;;
+esac
 
 SUDO=
 if command -v sudo >/dev/null; then
     SUDO=sudo
-    log_info "Sudo functional. Beginngin system installation"
+    log_info "Sudo functional. Starting system installation"
 elif [ "$EUID" -eq 0 ]; then
-    log_info "Root permissions. Beginngin system installation"
+    log_info "Root permissions. Starting system installation"
 else
     log_info "Sudo not functional and not root. continue with user permissions"
 fi
+
 rm index.sh THIRD_PARTY_LICENSES NOTICE.md soci-snapshotter-grpc
-# wget https://github.com/${SOCI_USER}/${SOCI_REPO}/releases/download/${LATEST_VERSION}/${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz && tar -xvf ${SOCI_REPO}-$(echo "$LATEST_VERSION" | sed 's/^v//')-linux-amd64.tar.gz && cp ./soci /usr/local/bin && cp ./soci-snapshotter-grpc /usr/local/bin
+
 # sudo ctr i pull --user AWS:$REGISTRY_PASSWORD $REPOSITORY_TAG
 sudo ctr i pull docker.io/library/nginx:latest
 sudo soci create docker.io/library/nginx:latest
-# ctr i pull --user AWS:$REGISTRY_PASSWORD $REPOSITORY_TAG
 
 # soci create $REPOSITORY_TAG
 
